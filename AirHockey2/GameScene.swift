e281d646daf5ee78e9ff58c8905bba558a584028
student2 <student2@jh-mba158-17.local>
2017-06-08 07:42:37 -0500

    edited the gamescene

--- AirHockey2/GameScene.swift
+++ AirHockey2/GameScene.swift
@@ -103,11 +103,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 reset()
             }
             else if backToMainOnBarNode.contains(location) && backToMainOnBarNode.alpha == 1 {
-                var viewControllerForSegue = self.view?.window?.rootViewController
+                let viewControllerForSegue = self.view?.window?.rootViewController
                 viewControllerForSegue?.dismiss(animated: true, completion: nil)
             }
             else if backToMainNode.contains(location) && backToMainNode.alpha == 1 {
-                var viewControllerForSegue = self.view?.window?.rootViewController
+                let viewControllerForSegue = self.view?.window?.rootViewController
                 viewControllerForSegue?.dismiss(animated: true, completion: nil)
             }
             else if playAgainNode.contains(location) && playAgainNode.alpha == 1 {
@@ -135,11 +135,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 reset()
             }
             else if backToMainOnBarNode.contains(location) && backToMainOnBarNode.alpha == 1 {
-                var viewControllerForSegue = self.view?.window?.rootViewController
+                let viewControllerForSegue = self.view?.window?.rootViewController
                 viewControllerForSegue?.dismiss(animated: true, completion: nil)
             }
             else if backToMainNode.contains(location) && backToMainNode.alpha == 1 {
-                var viewControllerForSegue = self.view?.window?.rootViewController
+                let viewControllerForSegue = self.view?.window?.rootViewController
                 viewControllerForSegue?.dismiss(animated: true, completion: nil)
             }
             else if playAgainNode.contains(location) && playAgainNode.alpha == 1 {
